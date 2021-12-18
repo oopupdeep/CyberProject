b186c464da304e5f4a9a88be2b1741f607dbe543
oopupdeep <ziwangkuan@126.com>
2021-12-18 19:54:00 +0800

    增加脚本展示以及修改功能

--- YamlReader.py
+++ YamlReader.py
@@ -5,11 +5,17 @@ class YamlReader:
     def get_yaml(self,yaml_file):
         print("***获取yaml数据***")
         file = open(yaml_file, 'r', encoding='utf-8')
-        file_data = file.read()
+        file_data = file.readlines()
         print("***转化yaml数据为字典或列表***")
-        data = yaml.load(file_data,Loader=yaml.FullLoader)
-        return data
+        return file_data
 
+# testYamlReader
+if __name__ == '__main__':
+    reader = YamlReader()
+    data = reader.get_yaml("YamlConfig/rip/rip_configA.yaml")
+    for lines in data:
+        # data = telnetClient.exec_cmd(lines)
+        print(lines)
 
 
 
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from TelnetClient import TelnetClient
 from flask import request,url_for
 from YamlReader import YamlReader
+import json
 
 app = Flask(__name__)
 telnetClient = TelnetClient()
@@ -82,21 +83,39 @@ def simpleExecuteStaticConfig():
         print(data)
     return "success"
 
-@app.route("showYaml", method=['POST'])
+@app.route("/showYaml", methods=['POST'])
 def showYaml():
-    yaml_file_name = request.get("yaml_file")
+    yaml_file_name = json.loads(request.get_data())["YamlName"]
     yaml_file = None
     if "static" in yaml_file_name:
         yaml_file = yamlReader.get_yaml("YamlConfig/static_router/{}".format(yaml_file_name))
     elif 'rip' in yaml_file_name:
         yaml_file = yamlReader.get_yaml("YamlConfig/rip/{}".format(yaml_file_name))
-    return yaml_file
+    elif 'ospf' in yaml_file_name:
+        yaml_file = yamlReader.get_yaml("YamlConfig/rip/{}".format(yaml_file_name))
+    print(json.dumps({"command":yaml_file}))
+    return json.dumps({"command":yaml_file})
 
 
 
-@app.route("modifyYaml", method=['POST'])
+@app.route("/modifyYaml", methods=['POST'])
 def modifyYaml():
-    yaml_file = request.get("yaml_file")
+    yaml_file = json.loads(request.get_data())["newFile"]
+    file_name = json.loads(request.get_data())["option"]
+    # yaml_file = yaml_file.split("\n")
+    try:
+        file = None
+        if "static" in file_name:
+            file = open("YamlConfig/static_router/{}".format(file_name),'w')
+        elif 'rip' in file_name:
+            file = open("YamlConfig/rip/{}".format(file_name),'w')
+        elif 'ospf' in file_name:
+            file = open("YamlConfig/ospf/{}".format(file_name),'w')
+        file.write(yaml_file)
+        file.close()
+        return json.dumps({"msg":"成功！"})
+    except:
+        return json.dumps({"msg":"修改失败！"})
 
 # @app.route("/executeRipConfig", methods=["POST"])
 # def executeRipConfigA():
